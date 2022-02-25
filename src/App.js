2ea45c9ec27ef2b06b91f711fa6a80f93ed81a1d
bahadir <baha_i@hotmail.com>
2022-02-25 15:39:45 +0100

    search erstellt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Login from './components/Login';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!</h1>
+     <Login/>
+     <br/>
+     <Search/>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Search</Form.Label>
+        <Form.Control type="password" placeholder="Search" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Search
+      </Button>
+    </Form>
+  );
+};
 
 export default Search;
