38911ba1128b65006858aae082fb15a22e8c6389
bahadir <baha_i@hotmail.com>
2022-02-25 18:15:58 +0100

    states erstellt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
 import './App.css';
 import Login from './components/Login';
 import Search from './components/Search';
 import Cards from './components/Cards';
 
 function App() {
+
+  const appID = "29dba890";
+  const appKey = "a5cf72ed0dabf9f3f0651254233f61ae";
+  const [movie, setMovie] = useState([]);
+  const [search, setSearch] = useState("");
+  const [request, setRequest] = useState("");
+
   return (
     <div className="App">
      <Login/>
