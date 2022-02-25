4476aa5b13cc850664d1ccdbf7b48c40ab0c78d4
bahadir <baha_i@hotmail.com>
2022-02-25 18:53:47 +0100

    api verbindung problem

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import './App.css';
-import Login from './components/Login';
+// import Login from './components/Login';
 import Search from './components/Search';
-import Cards from './components/Cards';
+
 
 function App() {
 
-  const appID = "29dba890";
-  const appKey = "a5cf72ed0dabf9f3f0651254233f61ae";
-  const [movie, setMovie] = useState([]);
-  const [search, setSearch] = useState("");
-  const [request, setRequest] = useState("");
 
-  useEffect(() => {
-    handlemovies();
-  },[request]);
-
-  const handlemovies = async () => {
-    const answer = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${appKey}`);
-    const data = await answer.json();
-    setMovie(data)
-
-  }
-
-  const handleSearch = (e) => {
-    setSearch(e.target.value)
-  }
-
-  const desiredMovie = (e) => {
-    e.preventDefault();
-    setRequest(search);
-    setSearch("");
-
-  }
 
   return (
     <div className="App">
-     <Login/>
-     <br/>
+     
      <Search/>
-     <br/>
-     <Cards/>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,53 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/search.css";
+// import Cards from './components/Cards';
 
 const Search = () => {
+
+  const appKey = "a5cf72ed0dabf9f3f0651254233f61ae";
+
+  const [movie, setMovie] = useState([]);
+  const [search, setSearch] = useState("");
+  const [request, setRequest] = useState("");
+
+  useEffect(() => {
+    handlemovies();
+  },[request]);
+
+  const handlemovies = async () => {
+    const answer = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${appKey}`);
+    const data = await answer.json();
+    setMovie(data)
+
+  }
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const desiredMovie = (e) => {
+    e.preventDefault();
+    setRequest(search);
+    setSearch("");
+
+  }
   return (
-    <Form className="search-form">
+    <Form className="search-form" onSubmit={desiredMovie}>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Search</Form.Label>
-        <Form.Control type="password" placeholder="Search" />
+        <Form.Control type="text" placeholder="Search" value={search} onChange={handleSearch}/>
       </Form.Group>
       <Button variant="primary" type="submit">
         Search
       </Button>
+      <div className="movies">
+        {movie.map((item, index) => (
+          <li key={index}>{item}</li>
+        ) )}
+
+      </div>
     </Form>
   );
 };
